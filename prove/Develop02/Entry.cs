981a3c46a3d98c27cca4052d9cae2dc0571ac6f6
lionel-alvarado16 <lionel.alex64@hotmail.com>
2024-09-28 12:46:11 -0500

    Design Completed

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,11 @@
+public class Entry
+{
+    public string _date;
+    public string promptText;
+    public string entryText;
+
+    public void Display()
+    {
+        
+    }
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        // Console.WriteLine("Hello Develop02 World!");
+
+        Console.WriteLine("Welcome to the Journal Program!");
+        Console.WriteLine("Please select one of the following choices:");
+
+        string userInput = "";
+
+        do
+        {
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+
+            Console.Write("What would you like to do? ");
+            userInput = Console.ReadLine();
+
+            Journal userJournal = new Journal();
+
+            if (userInput == "1")
+            {
+                userJournal.AddEntry();
+            }
+            else if (userInput == "2")
+            {
+                userJournal.DisplayAll();
+            }
+            else if (userInput == "3")
+            {
+                userJournal.SaveToFile();
+            }
+            else if (userInput == "4")
+            {
+                userJournal.LoadFromFile();
+            }
+
+        } while (userInput != "5");
     }
 }
