f8e43dd8bc42999cb6778577e0f1ce77b9d90659
lionel-alvarado16 <lionel.alex64@hotmail.com>
2024-10-06 01:18:13 -0500

    Design02 Completed

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+    }
+
+    public void Hide()
+    {
+
+    }
+
+    public void Show()
+    {
+
+    }
+
+    public bool IsHidden()
+    {
+        return false;
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+}
