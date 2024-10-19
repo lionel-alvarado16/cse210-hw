f5f8eed8736b38c7be6aafa32f333313e2db6e01
lionel-alvarado16 <lionel.alex64@hotmail.com>
2024-10-18 22:02:26 -0500

    Foundation1 completed

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -8,22 +8,30 @@ class Program
 
         // Comments of video 1
         List<Comments> commentsVideo1 = [
-            new Comments("gatonator.", "The song of quarantine Time"),
-            new Comments("antonikarbowski79", "Still makes me cry now at 45yrs old."),
-            new Comments("Yoongs_Tangerine", "How is this not viral? Listening to this till 2024"),
-            new Comments("nqtduc", "I love how each different generation has varied vibes of songs. Music is so unique that makes it beautiful and never boring")
+            new Comments("chopstick134", "Cleanest trailer ever. Hope to see this type of inovation in other devices too. Im looking forward and im excited"),
+            new Comments("halo-halo9244", "Watching this on my 720p phone. It's so sharp I can see every detail."),
+            new Comments("joshinglyyy", "proud to have this as my TV"),
+            new Comments("phantomlm", "I have this TV and It's extremely fast and the ai upscaling looks amazing, I switch between Amazon prime and yt and it does it in like 2 seconds and I can't wait to connect my Xbox series s to it")
             ];
 
         // Comments of video 2
         List<Comments> commentsVideo2 = [
-            new Comments("cliffbehum-nd2lp", "It's March 12, 2024 and I just found out that Eric Carmen died. Part of My youth in Cleveland. Rest in peace."),
-            new Comments("yukkaplant1", "So sad to hear the news.... such a talent. It's horrible reaching that age where voices from your youth are being lost so often."),
-            new Comments("tammyblalock3694", "I'm very lonely right now but Jesus is with me"),
+            new Comments("muzongooo", "This is why Sony has become the leader of the camera industry. Always pushing the limits: Eye AF, AI AF, and now Global Shutter!"),
+            new Comments("Efficient7x", "Sony's implementation of a global shutter is truly a remarkable feat in the camera industry!"),
+            new Comments("MrCoolAttitude", "As a Nikon shooter I'm always envious of Sony's technology innovations."),
+            ];
+
+        List<Comments> commentsVideo3 = [
+            new Comments("itsaty7760", "Finally a rbg fridge, I hope next year Lg will design a rbg toaster that razer never delivered"),
+            new Comments("Roadman1000", "Hopefully my appliance store chain gets one of these.  It would be a great center piece!"),
+            new Comments("X-3K", "This has got to be the most Aperture Science Portal 2 style home appliance i have ever seen, where can i get one?"),
+            new Comments("braden7272", "What's the dimensions of this fridge? Where can I buy it in Malaysia?")
             ];
 
         List<Video> videos = [
-            new Video("Close to you", "Carpenters", 273, commentsVideo1),
-            new Video("All By My Self", "Eric Carmen", 300, commentsVideo2)
+            new Video("2024 Neo QLED 8K: A New Era of Samsung AI TV", "Samsung", 169, commentsVideo1),
+            new Video("Introducing Alpha 9 III - Sony - α", "Sony Camera Channel", 134, commentsVideo2),
+            new Video("LG at CES 2024 : LG Refrigerator with MoodUP", "LG Global", 41, commentsVideo3)
             ];
         
         for (int i = 0; i < videos.Count; i++)
