87e1269c10c9b8e5f04141dcd547b94314070c8f
lionel-alvarado16 <lionel.alex64@hotmail.com>
2024-10-26 18:10:21 -0500

    Learning06 Completed

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        // Console.WriteLine("Hello Learning06 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Green", 2));
+        shapes.Add(new Rectangle("Red", 4, 5));
+        shapes.Add(new Circle("Yellow", 3));
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
